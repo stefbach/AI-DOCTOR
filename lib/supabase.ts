d13150b37e8c2680da9fd5f9024eb8593180dbac
stefbach <sbach1964@gmail.com>
2025-07-23 17:37:27 +0400

    Create lib/supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,8 @@
+// lib/supabase.ts
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
